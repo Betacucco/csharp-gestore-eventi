124bac97789f4947c089f432979e514c9db7c45b
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-22 12:22:11 +0200

    Milestone 1 e 2 senza try-catch

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        //ATTRIBUTI
+        private string titolo;
+        private DateTime dataEvento;
+        private int capienzaMaxEvento;
+        private int numeroPostiPrenotati = 0;
+
+        //COSTRUTTORE
+        public Evento(string titolo, DateTime dataEvento, int capienzaMaxEvento, int numeroPostiPrenotati)
+        {
+            SetTitolo(titolo);
+            SetDataEvento(dataEvento);
+            this.capienzaMaxEvento = capienzaMaxEvento;
+            this.numeroPostiPrenotati = numeroPostiPrenotati;
+        }
+
+        //GETTERS
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+
+        public DateTime GetDataEvento()
+        {
+            return dataEvento;
+        }
+
+        public int GetCapienzaMaxEvento()
+        {
+            return capienzaMaxEvento;   
+        }
+
+        public int GetNumeroPostiPrenotati()
+        {
+            return numeroPostiPrenotati;
+        }
+        //---------------------------------------------
+
+        //SETTERS
+        public void SetTitolo(string titolo)
+        {
+            if (string.IsNullOrEmpty(titolo))
+            {
+                throw new ArgumentNullException("Titolo non inserito!");
+            }
+            else
+            {
+                this.titolo = titolo;
+            }
+        }
+        
+        public void SetDataEvento(DateTime data)
+        {
+            if (data <= DateTime.Now)
+            {
+                throw new InvalidDataException("La data inserita e' nel passato o oggi.");
+            }
+            else
+            {
+                this.dataEvento = data;
+            }
+        }
+        //---------------------------------------------------
+
+        //Metodo per aggiungere posti
+        public int PrenotaPosti(int postiDaAggiugnere)
+        {
+            if (this.numeroPostiPrenotati >= this.capienzaMaxEvento || this.dataEvento <= DateTime.Now)
+            {
+                throw new Exception("I posti sono finiti o la data e' sbagliata!");
+            } else
+            {
+                this.numeroPostiPrenotati += postiDaAggiugnere;
+                return this.numeroPostiPrenotati;
+            }
+        }
+
+        //Metodo per togliere posti
+        public int DisdiciPosti(int postiDaTogliere)
+        {
+            if (this.numeroPostiPrenotati > 0 || this.dataEvento > DateTime.Now)
+            {
+                this.numeroPostiPrenotati -= postiDaTogliere;
+                return this.numeroPostiPrenotati;
+            } else
+            {
+                throw new Exception("Non puoi togliere posti perche' non ci sono o la data e' sbagliata!");
+            }
+        }
+
+        //Override del metodo toString per stampare ogni appuntamento in lista
+        public override string ToString()
+        {
+            string rappresentazioneInStringa = "";
+
+            rappresentazioneInStringa += "---- Evento ---- \n";
+            rappresentazioneInStringa += "Titolo evento: " + this.titolo;
+            rappresentazioneInStringa += " - ";
+            rappresentazioneInStringa += "Data Evento: " + this.dataEvento.ToString("dd/MM/yyyy")+ "\n";
+
+            return rappresentazioneInStringa;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using GestoreEventi;
+List<Evento> listaEventi = new List<Evento>();
+
+Console.Write("Inserisci nuemro di eventi: ");
+int numeroEventi = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numeroEventi; i++)
+{
+    Console.Write("Inserisci il nome dell'evento: ");
+    string nomeEvento = Console.ReadLine();
+
+    Console.Write("Inserisci data evento: ");
+    DateTime dataOra = DateTime.Parse(Console.ReadLine());
+
+    Console.Write("Inserisci la capienza massima: ");
+    int capienzaMassima = int.Parse(Console.ReadLine());
+
+    Console.Write("Quanti posti desideri prenotare? ");
+    int numeroPostiDaPrenotare = int.Parse(Console.ReadLine());
+
+    listaEventi.Add(new Evento(nomeEvento, dataOra, capienzaMassima, numeroPostiDaPrenotare));
+}
+
+foreach (Evento evento in listaEventi)
+{
+    Console.WriteLine();
+    Console.WriteLine("Numero di posti prenotati = " + evento.GetNumeroPostiPrenotati());
+    Console.WriteLine("Numero di posti disponibili = " + (evento.GetCapienzaMaxEvento() - evento.GetNumeroPostiPrenotati()));
+    
+    bool flag = true;
+    while (flag)
+    {
+        Console.WriteLine();
+        Console.Write("Vuoi disdire dei posti(si/no)? ");
+        string scelta = Console.ReadLine();
+
+
+        if (scelta == "si")
+        {
+            Console.Write("Inserisci numero di posti da disdire = ");
+            int numeroPostiDaDisdire = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Numero di posti prenotati = " + evento.DisdiciPosti(numeroPostiDaDisdire));
+            Console.WriteLine("Numero di posti disponibili = " + (evento.GetCapienzaMaxEvento() - evento.GetNumeroPostiPrenotati()));
+        } else if (scelta == "no")
+        {
+            Console.WriteLine("Ok va bene!");
+            flag = false;
+
+            Console.WriteLine();
+            Console.WriteLine("Numero di posti prenotati = " + evento.GetNumeroPostiPrenotati());
+            Console.WriteLine("Numero di posti disponibili = " + (evento.GetCapienzaMaxEvento() - evento.GetNumeroPostiPrenotati()));
+        } else
+        {
+            Console.WriteLine("Non hai scelto si o no. Scegli!");
+        }
+    }
+}
